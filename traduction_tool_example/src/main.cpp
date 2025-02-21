fa1a091766ee2ff9490f34789e1fe21b04f5ba4a
SonGoku-2000 <pablontxs@gmail.com>
2025-02-21 13:59:23 -0500

    Example regular bg items added

--- traduction_tool_example/src/main.cpp
+++ traduction_tool_example/src/main.cpp
@@ -3,17 +3,19 @@
 #include "bn_keypad.h"
 #include "bn_sprite_text_generator.h"
 #include "bn_sprite_ptr.h"
+#include "bn_regular_bg_ptr.h"
 
 #include "common_info.h"
 #include "common_variable_8x16_sprite_font.h"
 
 #include "traduction_sprite_language_flags.hpp"
+#include "traduction_regular_bg_language_flags_big_regular.hpp"
 #include "traduction_languages.hpp"
 
 
 void sprite_item_scene(bn::sprite_text_generator& text_generator) {
     constexpr bn::string_view info_text_lines[] = {
-        "A: next language",
+        "A: next flag",
         "",
         "START: go to next scene",
     };
@@ -47,6 +49,43 @@ void sprite_item_scene(bn::sprite_text_generator& text_generator) {
     }
 }
 
+void regular_bg_scene(bn::sprite_text_generator& text_generator) {
+    constexpr bn::string_view info_text_lines[] = {
+        "A: next flag",
+        "",
+        "START: go to next scene",
+    };
+
+    common::info info("Regular bg", info_text_lines, text_generator);
+
+    traduction::languages language = traduction::languages::SPANISH;
+
+    bn::regular_bg_item regular_bg_item = traduction::regular_bg_items::language_flags_big_regular(language);
+
+    bn::regular_bg_ptr regular_bg(regular_bg_item.create_bg());
+
+    while (!bn::keypad::start_pressed()) {
+
+        if (bn::keypad::a_pressed()) {
+            if (language == traduction::languages::SPANISH) {
+                language = traduction::languages::ENGLISH;
+            }
+            else if (language == traduction::languages::ENGLISH) {
+                language = traduction::languages::JAPANESE;
+            }
+            else if (language == traduction::languages::JAPANESE) {
+                language = traduction::languages::SPANISH;
+            }
+            regular_bg_item = traduction::regular_bg_items::language_flags_big_regular(language);
+            regular_bg.set_item(regular_bg_item);
+        }
+
+        info.update();
+        bn::core::update();
+    }
+}
+
+
 int main() {
     bn::core::init();
 
@@ -54,6 +93,9 @@ int main() {
 
     while (true) {
         sprite_item_scene(text_generator);
+        bn::core::update();
+
+        regular_bg_scene(text_generator);
         bn::core::update();
     }
 }
